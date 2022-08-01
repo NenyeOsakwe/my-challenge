4a00cc619d3a1868b7d49a4bd5be2a3ffe50173d
NenyeOsakwe <110306936+NenyeOsakwe@users.noreply.github.com>
2022-08-01 05:06:03 +0100

    Updated index.html and added CSS and JS files

---
+++ src/app.js
@@ -0,0 +1,19 @@
+function isWindy(speed) {
+  return speed > 5;
+}
+// function isWindy(speed) {
+//   return speed > 5;
+// }
+
+let lisbonSpeed = 10;
+if (isWindy(lisbonSpeed)) {
+  alert('it is windy');
+} else {
+  alert('it is not windy');
+}
+// let lisbonSpeed = 10;
+// if (isWindy(lisbonSpeed)) {
+//   alert('it is windy');
+// } else {
+//   alert('it is not windy');
+// }
